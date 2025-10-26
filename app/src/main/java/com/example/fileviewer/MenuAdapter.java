29855b746d36e3ffca9a9500afbd692cb839f448
admin <admin@DESKTOP-E07VVT6>
2025-10-26 17:43:27 +0500

    Разработанный дизайн главной страницы

---
+++ app/src/main/java/com/example/fileviewer/MenuAdapter.java
@@ -0,0 +1,9 @@
+package com.example.fileviewer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+public class MenuAdapter {
+}
